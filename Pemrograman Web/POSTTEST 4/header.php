27fc3b81368672a43cd2367eb3b38a3d1eca6289
Muhammad Alif <129609799+miezlearning@users.noreply.github.com>
2023-10-11 01:41:04 +0800

    Add files via upload

---
+++ Pemrograman Web/POSTTEST 4/header.php
@@ -0,0 +1,20 @@
+<header>
+<nav class="navbar">
+    <div class="logo">
+      <img src="Gambar/Shopkeepers_Logo.png" alt="Minecraft Logo">
+    </div>
+    <ul class="menu">
+      <li><a href="#produks">Beranda</a></li>
+      <li><a href="">About<A/a></li>
+      <li class="dropdown">
+        <a href="#produks">Produk</a>
+        <div class="dropdown-content">
+          <a href="#skin1">Skin 1</a>
+          <a href="#skin2">Skin 2</a>
+        </div>
+      </li>
+      <li><a href="#kontak">Kontak</a></li>
+      <li><button id="mode-toggle"><i class="fas fa-sun"></i></button></li>
+    </ul>
+  </nav>    
+</header>
