af6a718e3b3ca50038a8c510b8976740c5436b0f
Muhammad Alif <129609799+miezlearning@users.noreply.github.com>
2023-11-16 22:16:15 +0800

    Delete TEST HOSTING directory

--- TEST HOSTING/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-    <div id="footer" class="footer-content">
-      <p>&copy; 2023 Toko Skin Minecraft</p>
-      <p>Alamat: Jalan Minecraft, Blok A, No. 123</p>
-      <p>Email: info@tokoskinminecraft.com</p>
-    </div>
-</footer>
--- TEST HOSTING/readme.md
+++
@@ -1,7 +0,0 @@
-Berikut Videonya : 
-
-
-
-
-https://github.com/miezlearning/Kuliah/assets/129609799/417d0ea5-307d-4edd-9a98-d918f1401fad
-
